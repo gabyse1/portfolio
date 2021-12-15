e22b056350ad7040fa29a854e3b706f5e4fa988d
iamgaby <gaby.es.iaa@gmail.com>
2021-12-15 11:52:06 -0500

    Add card information dinamicaly in main.js

--- scripts/main.js
+++ scripts/main.js
@@ -29,22 +29,22 @@ function createCard(imageMobile, imageDesktop, title, titleDesktop, shortDescrip
 }
 
 const postsDisplay = createCard('images/img-placeholder-display.png', 'images/img-placeholder-display.png', 'Multi-Post Stories',
-'Multi-Post Stories', `A daily selection of privately personalized reads; no accounts or sign-ups required. 
+  'Multi-Post Stories', `A daily selection of privately personalized reads; no accounts or sign-ups required. 
 has been the industry's standard dummy text ever since the 1500s, when an unknown printer 
 took a standar dummy text.`, `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
 Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer
 took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,
 but also the leap into electronic typesetting, remaining essent`, ['css', 'html', 'bootstrap', 'Ruby'],
-'https://gabyse1.github.io/repo-portfolio', 'https://github.com/gabyse1/repo-portfolio');
+  'https://gabyse1.github.io/repo-portfolio', 'https://github.com/gabyse1/repo-portfolio');
 cards.push(postsDisplay);
 
 const special = createCard('images/img-placeholder-articles.png', 'images/img-placeholder-articles-01.png', 'Profesional Art Printing Data',
-'Profesional Art Printing Data', `A daily selection of privately personalized reads; no accounts or sign-ups required. 
+  'Profesional Art Printing Data', `A daily selection of privately personalized reads; no accounts or sign-ups required. 
 has been the industry's standard`, `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
 Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer
 took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,
 but also the leap into electronic typesetting, remaining essent`, ['html', 'bootstrap', 'Ruby'],
-'https://gabyse1.github.io/repo-portfolio', 'https://github.com/gabyse1/repo-portfolio');
+  'https://gabyse1.github.io/repo-portfolio', 'https://github.com/gabyse1/repo-portfolio');
 special.id = 'special';
 cards.push(special);
 
@@ -53,33 +53,71 @@ const titles = ['Data Dashboard Healthcare', 'Website Portfolio', 'Profesional A
 let index = 2;
 
 while (idCount <= 5) {
-  if (index > 4){
+  if (index > 4) {
     index = 2;
   }
-  const other = createCard('images/img-placeholder-articles.png', `images/img-placeholder-articles-0${index}`, 'Profesional Art Printing Data', 
-  `${titles[index]}`,
+  const other = createCard('images/img-placeholder-articles.png', `images/img-placeholder-articles-0${index}`, 'Profesional Art Printing Data',
+    `${titles[index]}`,
     `A daily selection of privately personalized reads; no accounts or sign-ups required. 
   has been the industry's standard`, `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer
  took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,
  but also the leap into electronic typesetting, remaining essent`, ['html', 'bootstrap', 'Ruby'],
- 'https://gabyse1.github.io/repo-portfolio', 'https://github.com/gabyse1/repo-portfolio');
+    'https://gabyse1.github.io/repo-portfolio', 'https://github.com/gabyse1/repo-portfolio');
   cards.push(other);
   index += 1;
 }
 
 const container = document.querySelector('.works-container');
 
-function render () {
-  cards.forEach( cardobject =>  {
+function render() {
+  const display = document.createElement('div');
+  display.classList.add('posts-display');
+  container.appendChild(display);
+
+  const list = document.createElement('div');
+  list.classList.add('posts-list');
+  container.appendChild(list);
+
+  cards.forEach(cardobject => {
+
+    const figure = document.createElement('figure');
+    figure.classList.add('post-image');
+
+    const mobImg = document.createElement('img');
+    mobImg.classList.add('mobile-item');
+    mobImg.setAttribute('src', cardobject.imageMobile);
+    mobImg.setAttribute('alt', 'card image');
+    figure.appendChild(mobImg);
+
+    const desktopImg = document.createElement('img');
+    desktopImg.classList.add('desktop-item');
+    desktopImg.setAttribute('src', cardobject.imageDesktop);
+    desktopImg.setAttribute('alt', 'card image');
+    figure.appendChild(desktopImg);
+
+    // card information content
+    const info = document.createElement('div');
+    info.classList.add('post-information');
+    const infoContent = ``;
+
+
     if (cardobject.id == 'card-1') {
-      const Display = document.createElement('div');
-      Display.classList.add('posts-display');
-      container.appendChild(Display);
+      display.appendChild(figure);
     }
     else {
-      const list = document.createElement('div');
-      list.classList.add();
+      const card = document.createElement('article');
+      card.classList.add('post-article');
+      if (cardobject.id === 'special') {
+        card.classList.add('active-article');
+        info.classList.add('mobile-item');
+      }
+      list.appendChild(card);
+      card.appendChild(figure);
+
+      const postcaption = document.createElement('figcaption');
+      postcaption.classList.add('post-caption');
+
     }
   })
 }
