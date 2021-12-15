c6b9a75b1964d657e40fc66d061d189b8e4c78ce
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2021-12-15 10:40:30 -0500

    Add object factory function with the required values for the project

--- scripts/main.js
+++ scripts/main.js
@@ -11,18 +11,59 @@ toggleButtons.forEach((button) => {
 let cards = [];
 let idCount = 0;
 
-function createCard(imageMobile, title, shortDescription,
+function createCard(imageMobile, imageDesktop, title, titleDesktop, shortDescription,
   longDescription, tags, linkLive, linkSource) {
-  counter += 1;
+  idCount += 1;
   return {
-    id: `card-${counter}`,
-    image,
+    id: `card-${idCount}`,
+    imageMobile,
+    imageDesktop,
     title,
+    titleDesktop,
     shortDescription,
     longDescription,
     tags,
     linkLive,
     linkSource,
-    desktop,
   };
+}
+
+const postsDisplay = createCard('images/img-placeholder-display.png', 'images/img-placeholder-display.png', 'Multi-Post Stories',
+'Multi-Post Stories', `A daily selection of privately personalized reads; no accounts or sign-ups required. 
+has been the industry's standard dummy text ever since the 1500s, when an unknown printer 
+took a standar dummy text.`, `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer
+took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+but also the leap into electronic typesetting, remaining essent`, ['css', 'html', 'bootstrap', 'Ruby'],
+'https://gabyse1.github.io/repo-portfolio', 'https://github.com/gabyse1/repo-portfolio');
+cards.push(postsDisplay);
+
+const special = createCard('images/img-placeholder-articles.png', 'images/img-placeholder-articles-01.png', 'Profesional Art Printing Data',
+'Profesional Art Printing Data', `A daily selection of privately personalized reads; no accounts or sign-ups required. 
+has been the industry's standard`, `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer
+took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+but also the leap into electronic typesetting, remaining essent`, ['html', 'bootstrap', 'Ruby'],
+'https://gabyse1.github.io/repo-portfolio', 'https://github.com/gabyse1/repo-portfolio');
+special.id = 'special';
+cards.push(special);
+
+const titles = ['Data Dashboard Healthcare', 'Website Portfolio', 'Profesional Art Printing Data'];
+
+let index = 2;
+
+while (idCount <= 5) {
+  if (index > 4){
+    index = 2;
+  }
+  const other = createCard('images/img-placeholder-articles.png', `images/img-placeholder-articles-0${index}`, 'Profesional Art Printing Data', 
+  `${titles[index]}`,
+    `A daily selection of privately personalized reads; no accounts or sign-ups required. 
+  has been the industry's standard`, `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+ Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer
+ took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+ but also the leap into electronic typesetting, remaining essent`, ['html', 'bootstrap', 'Ruby'],
+ 'https://gabyse1.github.io/repo-portfolio', 'https://github.com/gabyse1/repo-portfolio');
+  cards.push(other);
+  index += 1;
 }
