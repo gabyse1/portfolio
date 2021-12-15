9b2f3b10eb3e68eddd703915741956ee6cbff557
iamgaby <gaby.es.iaa@gmail.com>
2021-12-15 10:13:45 -0500

    Add object variables to main.js

--- scripts/main.js
+++ scripts/main.js
@@ -7,3 +7,22 @@ const toggleButtons = document.querySelectorAll('.toggle');
 toggleButtons.forEach((button) => {
   button.addEventListener('click', toggleMenu);
 });
+
+let cards = [];
+let idCount = 0;
+
+function createCard(imageMobile, title, shortDescription,
+  longDescription, tags, linkLive, linkSource) {
+  counter += 1;
+  return {
+    id: `card-${counter}`,
+    image,
+    title,
+    shortDescription,
+    longDescription,
+    tags,
+    linkLive,
+    linkSource,
+    desktop,
+  };
+}
